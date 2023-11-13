b4cf91ed206e2a12e635bd72f4e6656e6b8bc584
Victor <Virtu_gatu5467@hotmail.com>
2023-11-13 19:25:58 -0300

    Alteração projeto web integrantes

--- src/app/Integrantes/page.jsx
+++ src/app/Integrantes/page.jsx
@@ -6,7 +6,7 @@ export default function Integrantes(){
             <p>RM:97850 - Gustavo Soares Fosaluza - 1TDSPL</p>
             <p>RM:551247 - Murilo Caumo Murgia</p>
             <p>RM:99577 - Guilherme Dias Gomes - 1TDSR</p>
-            <p>RM:99661 - Victor Augusto Cifontes de Oliveira</p>
+            <p>RM:99661 - Victor Augusto Cifontes de Oliveira- 1TDSPN</p>
             <h1><a href="https://github.com/Guilherme-Dias-gomes/SprintWEB" className="repositorio">| Acesse o nosso repositório |</a></h1>
         </main>
     )
