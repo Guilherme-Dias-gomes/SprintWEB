499d66de9eacd216dda481aa089e0fa38b0eea3a
Victor <Virtu_gatu5467@hotmail.com>
2023-11-13 19:24:03 -0300

    alteração projeto web cabecalho

--- src/app/Final/page.jsx
+++ src/app/Final/page.jsx
@@ -2,7 +2,7 @@ export default function Final(){
 
     return(
         <main>
-            <h1>Muito obrigado pelas validações!</h1>
+            <h1>Muito obrigado pelas validações!!</h1>
             <p>Em breve enviaremos um valor da cotação.</p>
         </main>
     )
--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,7 +1,7 @@
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
-      <h1 className="titulo">Faça o seguro da sua bicicleta de um jeito simples com a Luna</h1>
+      <h1 className="titulo">Faça o seguro da sua bicicleta de um jeito simples com a Luna para ter mais seguro e conforto</h1>
     </header>
   );
 }
